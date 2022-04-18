2bb9e4414b5a5d886c1f7684989ba0e723480399
kshashank2501 <kalyanshaan25@gmail.com>
2022-04-18 13:17:24 +0530

    first commit

---
+++ sample1.java
@@ -0,0 +1,11 @@
+package com.demo.pkg;
+
+public class sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is project1");
+
+	}
+
+}
